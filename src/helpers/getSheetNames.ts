7eb93e9093d54667d75ecb4579edd3c3c85a8ce3
Juan Carlos Almaguer Alfaro <jc_almaguer@outlook.com>
2024-01-30 15:12:52 -0600

    Se modificio getSheetNames para adaptarlo a AddGoogleSheets.

--- src/helpers/getSheetNames.ts
+++ src/helpers/getSheetNames.ts
@@ -1,25 +1,23 @@
 import axios from 'axios';
-import { useSelector } from 'react-redux/es/hooks/useSelector';
-import { store } from '../store/store';
-import { QrState, setLoading, setSpreadsheetPages } from '../store/qr/qrSlice';
-
+import { store } from '../store/store'; // Importa tu store de Redux
+import { setLoading, setSpreadsheetPages } from '../store/qr/qrSlice';
 
 export const getSheetNames = (dispatch: typeof store.dispatch) => {
-    const sheetsId = useSelector((state: QrState) => state.sheetsId); // Obtiene el ID de la hoja de cálculo del estado de Redux
+  const sheetsId = store.getState().qr.sheetsId; // Obtiene el ID de la hoja de cálculo del estado de Redux
 
-    const getDataUrl =
-      'https://script.google.com/macros/s/AKfycbyZWqsMPVumZDOjfUEXbUrApiwty7jpGPKomBMBTBGijPbAqDPuJtJ7kp3whoU9H_IU/exec?spreadsheetId=' +
-      sheetsId +
-      '&sheets=names';
-  
-    dispatch(setLoading(true));
-    axios
-      .get(getDataUrl)
-      .then((res) => {
-        dispatch(setSpreadsheetPages(res.data.sheetNames));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    dispatch(setLoading(false));
-  };
+  const getDataUrl =
+    'https://script.google.com/macros/s/AKfycbyZWqsMPVumZDOjfUEXbUrApiwty7jpGPKomBMBTBGijPbAqDPuJtJ7kp3whoU9H_IU/exec?spreadsheetId=' +
+    sheetsId +
+    '&sheets=names';
+
+  dispatch(setLoading(true));
+  axios
+    .get(getDataUrl)
+    .then((res) => {
+      dispatch(setSpreadsheetPages(res.data.sheetNames));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  dispatch(setLoading(false));
+};
