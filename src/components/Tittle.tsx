c2fce7d2e6e25fbde716a0e535d155c177eabc84
Juan Carlos Almaguer Alfaro <jc_almaguer@outlook.com>
2024-01-31 14:13:28 -0600

    Se modificó para realizar las funciones correspondientes y solo despues el reenderizado

--- src/components/Tittle.tsx
+++ src/components/Tittle.tsx
@@ -1,8 +1,11 @@
 import { View, Text, StyleSheet } from 'react-native';
-
+import { store, useAppDispatch, useAppSelector } from '../store/store';
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import qrSlice, { QrState } from '../store/qr/qrSlice';
+import { useLoadSheetTitle } from '../hooks/useLoadSheetTitle'; // Importa el hook personalizado
 
 export const Tittle = () => {
-
     const date = new Date();
 
     const month = date.toLocaleDateString('es-ES', { 
@@ -21,34 +24,49 @@ export const Tittle = () => {
 
     weekDay = weekDay.charAt(0).toUpperCase() + weekDay.slice(1);
 
+    const {sheetsTitle, loading} = useAppSelector(state => state.qr) // Obtiene el título de la hoja de cálculo del estado de Redux
+
+    useLoadSheetTitle(); // Utiliza el hook para obtener el título de la hoja de cálculo
+
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+
+    }, [sheetsTitle]);
+
     return (
         <View style={styles.container}>
-            <Text style={styles.header}>Gestión de Asistencias</Text>
-            <Text style={styles.date}>{weekDay} {day}, {month.charAt(0).toUpperCase() + month.slice(1)}</Text>
+            {
+                (loading) ?
+                    <Text>Loading</Text>:
+                    <><Text style={styles.header}>{sheetsTitle}</Text><Text style={styles.date}>{weekDay} {day}, {month.charAt(0).toUpperCase() + month.slice(1)}</Text></>
+            }
+            
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#fff',
-        alignItems: 'flex-start',
-        justifyContent: 'flex-start',
-        marginTop: '15%',
-        marginLeft: '5%',
-        border: 1,
-        //borderWidth: 5,
-        borderColor: 'red',
-        position: 'relative',
-    },
-    header: {
-        fontSize: 24,
-        fontWeight: '400',
-        marginBottom: 10,
-        color: '#8A3CB0',
-    },
-    date:{
-        fontSize: 18,
-        color: '#838383'
-    }
+  container: {
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    marginTop: '15%',
+    marginLeft: '5%',
+    border: 1,
+    //borderWidth: 5,
+    borderColor: 'red',
+    position: 'relative',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: '400',
+    marginBottom: 10,
+    color: '#8A3CB0',
+  },
+  date:{
+    fontSize: 18,
+    color: '#838383'
+  }
 });
+
