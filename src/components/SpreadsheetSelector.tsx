d54db7d76aeed61c581d6c86689efd9e4ca17e0b
Juan Carlos Almaguer Alfaro <jc_almaguer@outlook.com>
2024-01-31 15:26:38 -0600

    Se modificó SpreadSheetSelector para convertirlo en un picker que permita dejar espacio en el centro de la aplicación. Además se añadió un botón para poder ir al scanner de la hoja modificada.

--- src/components/SpreadsheetSelector.tsx
+++ src/components/SpreadsheetSelector.tsx
@@ -1,100 +1,71 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image, Alert, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image, Alert, SafeAreaView, Button } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 import { selectSpreadsheetPage } from '../store/qr/qrSlice';
 import { useAppDispatch, useAppSelector } from '../store/store';
 import { getSheetNames } from '../helpers';
 import { useLoadSheetNames } from '../hooks/useLoadSheetNames';
 
-
-type ItemProps = {
-    item: string;
-    onPress: () => void;
-    style: {
-        backgroundColor: string,
-        textColor: string,
-    }
-}
-
-const Item = ({ item, onPress, style }: ItemProps) => {
-
-    return (
-        //Contenedor de la lista de eventos.
-       <View style={styles.lista}>
-            <View style={[styles.item, { backgroundColor: style.backgroundColor }]}>
-                <Text style={[styles.title, { color: style.textColor }]}>{item}</Text>
-                <TouchableOpacity style={[styles.button]} onPress={onPress}>
-                    <Image
-                        style={styles.tinyLogo}
-                        source={require('../../assets/next.png')}
-                    />
-                </TouchableOpacity>
-            </View>
-        </View>
-        /*<View style={[styles.item, { backgroundColor: style.backgroundColor }]}>
-            <Text style={[styles.title, { color: style.textColor }]}>{item}</Text>
-            <TouchableOpacity style={[styles.button]} onPress={onPress}>
-                <Image
-                    style={styles.tinyLogo}
-                    source={require('../../assets/next.png')}
-                />
-            </TouchableOpacity>
-        </View>*/
-
-    )
-}
-
-
 export const SpreadsheetSelector = () => {
 
     const [selectedId, setSelectedId] = useState<string>();
+    const [showPicker, setShowPicker] = useState(false); // Nuevo estado para mostrar u ocultar el Picker
 
     const { spreadsheetPages, loading } = useAppSelector(state => state.qr);
 
     const dispatch = useAppDispatch();
     
-   const {fetchSheetNames} = useLoadSheetNames();
+    const {fetchSheetNames} = useLoadSheetNames();
 
     useEffect(() => {
 
     }, [spreadsheetPages])
 
-
-
-    const renderItem = ({ item }: { item: string }) => {
-        const backgroundColor = item === selectedId ? "#fff" : "#fff";
-        const color = item === selectedId ? 'white' : 'black';
-
-        return (
-            <Item
-                item={item}
-                onPress={() => {
-                    setSelectedId(item);
-                    dispatch(selectSpreadsheetPage(item));
-                }}
-                style={{ backgroundColor, textColor: color }}
-            />
-        );
-    };
-
     return (
         <View style={styles.container}>
-
             {
                 (loading) ?
                     <Text>Loading...</Text> :
-                    <FlatList
-                        onMomentumScrollEnd={fetchSheetNames}
-                        data={spreadsheetPages}
-                        renderItem={renderItem}
-                        keyExtractor={(item) => item}
-                        extraData={selectedId}
-                    />
+                    <View>
+                        <View style={styles.pickerContainer}>
+                            <Picker
+                                style={styles.picker}
+                                selectedValue={selectedId}
+                                onValueChange={(itemValue) => {
+                                    setSelectedId(itemValue);
+                                    //dispatch(selectSpreadsheetPage(itemValue));
+                                }}
+                            >
+                                <Picker.Item label="Selecciona una opción" value="" />
+                                {spreadsheetPages.map((item, index) => (
+                                    <Picker.Item key={index} label={item} value={item} />
+                                ))}
+                            </Picker>
+                        </View>
+                        { selectedId && (
+                            <TouchableOpacity
+                                style={{
+                                    backgroundColor: '#841584',
+                                    padding: 10,
+                                    borderRadius: 5,
+                                    alignSelf: 'center',
+                                    width: '95%'
+                                }}
+                                onPress={() => {
+                                    dispatch(selectSpreadsheetPage(selectedId));
+                                }}
+                            >
+                                <Text style={{color: 'white', textAlign: 'center'}}>Scannear</Text>
+                            </TouchableOpacity>
+                        )}
+                    </View>
             }
         </View>
     )
 }
 
+//Contenedor principal para la lista de elementos
 //Contenedor principal para la lista de elementos
 const styles = StyleSheet.create({
     container: {
@@ -105,7 +76,24 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
     },
+    pickerContainer: {
+        borderWidth: 1,
+        borderColor: '#CCCCCC',
+        borderRadius: 10,
+        backgroundColor: '#fff',
+        marginVertical: 10,
+        width: '95%',
+        alignSelf: 'center',
+    },
+    picker: {
+        height: 50,
+        width: '100%',
+        color: '#8A3CB0', // El color del texto en el Picker
 
+    },
+    picker_text: {
+        fontSize: 20,
+    },
     //Style para la lista de elementos
     lista: {
         border: 1,
@@ -137,6 +125,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         padding: 10,
         borderRadius: 10,
+        marginTop: '10%'
     },
 
     tinyLogo: {
@@ -144,5 +133,4 @@ const styles = StyleSheet.create({
         height: 25,
         objectFit: 'cover',
     },
-
 });
