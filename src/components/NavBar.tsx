1248d16ced3423d2829ede9e615fc0a5a2c016eb
Carlos <jc_almaguer@outlook.com>
2024-04-12 12:54:54 -0600

    Comments added to document the code.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,15 @@
+// IMPORTS
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
+// Interface fot the navigator options
 interface NavBarProps {
   onPress: () => void;
   onPressExit: () => void;
   onPressAdd: () => void;
 }
 
-const styles = StyleSheet.create({
-  navBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    backgroundColor: '#000', // fondo negro
-    padding: 10,
-  },
-  button: {
-    backgroundColor: '#6a0dad', // botones morados
-    padding: 10,
-    borderRadius: 5,
-  },
-  text: {
-    color: '#fff', // texto blanco
-    fontWeight: 'bold',
-  },
-});
-
+// Navigator's constructor
 export const NavBar: React.FC<NavBarProps> = ({ onPress, onPressAdd, onPressExit }) => {
   return (
     <View style={styles.navBar}>
@@ -41,4 +26,21 @@ export const NavBar: React.FC<NavBarProps> = ({ onPress, onPressAdd, onPressExit
   );
 };
 
-
+// Navigator's Styles
+const styles = StyleSheet.create({
+  navBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#fff',
+    padding: 10,
+  },
+  button: {
+    backgroundColor: '#6a0dad',
+    padding: 10,
+    borderRadius: 5,
+  },
+  text: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
